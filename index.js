60e8bdb976d545f1830019511523fa76b2ac9ba2
Andrii Mytsak <40431179+lenon777@users.noreply.github.com>
2019-07-09 00:48:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -24,3 +24,30 @@ function sort(arr){
 
 	 return first;
 };
+
+// Singleton pattern
+
+var DBconnection = (function(){
+	var uniqueConection;
+	function createConnection(driver,username,password,URL){
+		var uniqueConnection = new Object();		
+		uniqueConnection.driver = driver;
+		uniqueConnection.username = username;
+		uniqueConnection.password = password;
+		uniqueConnection.URL = URL;
+
+		return uniqueConnection;
+	}
+		return{
+			setConnection: function(driver,username,password,URL){
+			if(!uniqueConection){
+				uniqueConection = createConnection(driver,username,password,URL);
+			}
+			return uniqueConection;
+		}
+	}
+
+}());
+var sql = DBconnection.setConnection("sqlDriver","zorro","zorro124","http://www.zorro.com");
+var mongo = DBconnection.setConnection("mongoDriver","transformer","transformer124","http://www.transformer.com");
+console.log(sql === mongo); //true
